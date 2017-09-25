9b30cf09113a33f7773af402dea2a312a06a7471
WhalesAreNice <an1994xp@gmail.com>
2017-09-25 11:57:24 -0400

    Add files via upload

--- week3/app.js
+++ week3/app.js
@@ -1,9 +1,19 @@
 var points = 0;
 
 var answer1 = document.getElementById("60");
-var wrong = document.getElementsByClassName("wrong");
+var wrong1 = document.getElementsByClassName("wrong1");
 var message1 = document.getElementById("message1");
 var point1 = 0;
+var answer2 = document.getElementById("dog");
+var wrong2 = document.getElementsByClassName("wrong2");
+var message2 = document.getElementById("message2");
+var point2 = 0;
+var answer3 = document.getElementById("five");
+var wrong3 = document.getElementsByClassName("wrong3");
+var message3 = document.getElementById("message3");
+var point3 = 0;
+
+var points = point1 + point2 + point3;
 
 
 answer1.onclick = function() {
@@ -11,12 +21,33 @@ answer1.onclick = function() {
     message1.innerText = "";
 }
 
-for (let i=0; i < wrong.length; i++) {
-    wrong[i].onclick = function () {
-        message1.innerText = "Are you sure that's correct?";
+for (let i=0; i < wrong1.length; i++) {
+    wrong1[i].onclick = function () {
+        message1.innerText = "Don't forget the order of operations.";
     }
 }
 
+answer2.onclick = function() {
+    point2 = 1;
+    message2.innerText = "";
+}
+
+for (let i=0; i < wrong2.length; i++) {
+    wrong2[i].onclick = function () {
+        message2.innerText = "Try again!";
+    }
+}
+
+answer3.onclick = function() {
+    point3 = 1;
+    message3.innerText = "";
+}
+
+for (let i=0; i < wrong3.length; i++) {
+    wrong3[i].onclick = function () {
+        message3.innerText = "Are you sure that's correct?";
+    }
+}
 
 submit.onclick = function() {
 	var submit = document.getElementById("submit");
@@ -24,6 +55,6 @@ submit.onclick = function() {
    
 	var message = document.getElementById("message");
     
-    
+    message.innerText = "You've gotten " + points + " out of 3 correct, your score is " + 100/3*points;
     
 };                          
