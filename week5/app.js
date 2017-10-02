5b37c6d7bc319367619bcaf93eee065f2002fa1e
Allan Ng <allan.ng@f1006-30-34172.bmcc.cuny.edu>
2017-10-02 15:20:07 -0400

    group project commit

--- week5/app.js
+++ week5/app.js
@@ -1,26 +1,71 @@
-submit.onclick = function() {
+click1.onclick = function() {
     
-    var question = document.getElementById("question");
+    var click1 = document.getElementById("click1")
     
-    var answer = ["It is certain","It is decidedly so","Without a doubt","Yes definitely","You may rely on it","As I see it, yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very Doubtful"];
+    var message1 = ["an object","an interaction","a website","a service","an image","an app","an experience","an installation","a technology","a brand","a concept","a book"];
     
-	var submit = document.getElementById("submit");
+    var answer1 = document.getElementById("answer1");
     
-	var message = document.getElementById("message");
+    var display1 = message1[Math.floor(Math.random() * message1.length)];
     
-    var magic = answer[Math.floor(Math.random() * answer.length)];
+    answer1.innerText = display1; 
     
-    document.getElementById("8ball").style.display="block";
-    message.innerText = "";
-    
-    setTimeout(function(){ 
-        document.getElementById("8ball").style.display="none";
-        message.innerText = question.value + "\n\n" + magic; 
-    }, 3570);
-    
-    
-    
-};     
+};    
 
+click2.onclick = function() {
+    
+    var click2 = document.getElementById("click2")
+    
+    var message2 = ["circles","African fashion","squares","dots", "furniture", "cameras","natural materials","senior citizens","Eastern medicine","guns","healthcare","curves"];
+    
+    var answer2 = document.getElementById("answer2");
+    
+    var display2 = message2[Math.floor(Math.random() * message2.length)];
+    
+    answer2.innerText = display2; 
+    
+};    
 
+click3.onclick = function() {
+    
+    var click3 = document.getElementById("click3")
+    
+    var message3 = ["trusting","chaotic","approachable","discrete","tactical","collaborative","immersive","a family","paradigm shifting","overlapping","contrasting","responsive"];
+    
+    var answer3 = document.getElementById("answer3");
+    
+    var display3 = message3[Math.floor(Math.random() * message3.length)];
+    
+    answer3.innerText = display3; 
+    
+};    
+
+click4.onclick = function() {
+    
+    var click4 = document.getElementById("click4")
+    
+    var message4 = ["forms","brand touchpoints","patterns","wireframes","textures","materials","VR simulations","hardware hacks","concept sketches","personas","buttons","layouts"];
+    
+    var answer4 = document.getElementById("answer4");
+    
+    var display4 = message4[Math.floor(Math.random() * message4.length)];
+    
+    answer4.innerText = display4; 
+    
+};    
+
+click5.onclick = function() {
+    
+    var click5 = document.getElementById("click5")
+    
+    var message5 = ["fabric","pen \u0026 paper","paint","collage","video","code","card \u0026 tape","pipe cleaners","existing objects","CAD","Photoshop","Excel"];
+    
+    var answer5 = document.getElementById("answer5");
+    
+    var display5 = message5[Math.floor(Math.random() * message5.length)];
+    
+    answer5.innerText = display5; 
+    
+};    
+    
 
